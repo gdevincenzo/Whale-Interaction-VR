d472b580378f11c365f5262b8c6743b25e9b08bd
Gabriela De Vincenzo <gde.vincenzo17@gmail.com>
2019-01-29 14:53:24 -0800

    first commit

---
+++ Assets/Scripts/LookInteractable.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookInteractable : MonoBehaviour
+{
+    protected Material previousMat = null;
+    protected Vector3 interactorHitPosition = Vector3.zero;
+    protected bool selected = false;
+    public Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    public virtual void OnSelectEnter()
+    {
+        var meshRenderer = this.GetComponent<MeshRenderer>();
+        if (meshRenderer != null)
+        {
+            previousMat = meshRenderer.material;
+            meshRenderer.material = selectedMat;
+        }
+        selected = true;
+        anim.SetBool("seen", true);
+    }
+
+    public virtual void OnSelectExit()
+    {
+        var meshRenderer = this.GetComponent<MeshRenderer>();
+        if (meshRenderer != null && previousMat != null)
+        {
+            meshRenderer.material = previousMat;
+        }
+        selected = false;
+        anim.SetBool("seen", false);
+    }
+
+
+    public virtual void InteractorPosition(Vector3 position)
+    {
+        interactorHitPosition = position;
+    }
+
+    public virtual void OnInteract()
+    {
+        return;
+    }
+
+    public Material selectedMat = null;
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Roll-a-Ball-VR
+
+To launch the project in VR, open in unity on a computer attached to an oculus rift. 
+Press play, and use the arrowkeys to move the ball and the headset to see.
+
+Also includes Luna Interaction Scene. To launch, open scene and press play.
